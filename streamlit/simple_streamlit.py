0658fc6fe86e59cbc41dc1269ccc5a0121fd7bf6
Samwel Emmanuel <smakubi@Grentinas-MacBook-Pro-2.local>
2025-03-13 23:15:22 -0400

    x

--- streamlit/simple_streamlit.py
+++ streamlit/simple_streamlit.py
@@ -18,30 +18,30 @@ st.set_page_config(page_title="SQL and Python Agent")
 # MAIN PAGE
 st.title("SQL and Python Agent")
 
-# # 1. Initialize session state here.
-# if "db_config" not in st.session_state:
-#     st.session_state.db_config = {
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',
-#         'DATABASE': '',
-#         'PORT': '3306'
-#     }
+# 1. Initialize session state here.
+if "db_config" not in st.session_state:
+    st.session_state.db_config = {
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'DATABASE': '',
+        'PORT': '3306'
+    }
 
-# if "db_connected" not in st.session_state:
-#     st.session_state.db_connected = False
+if "db_connected" not in st.session_state:
+    st.session_state.db_connected = False
 
-# if 'databases' not in st.session_state:
-#     st.session_state.databases = []
+if 'databases' not in st.session_state:
+    st.session_state.databases = []
 
 
-# # SIDE BAR
-# st.sidebar.title("DATABASE CONFIGURATION")
-# st.sidebar.subheader("Enter MySQL connection details:", divider=True)
-# user = st.sidebar.text_input("User", value=st.session_state.db_config['USER'])
-# password = st.sidebar.text_input("Password", type="password", value=st.session_state.db_config['PASSWORD'])
-# host = st.sidebar.text_input("Host", value=st.session_state.db_config['HOST'])
-# port = st.sidebar.text_input("Port", value=st.session_state.db_config['PORT'])
+# SIDE BAR
+st.sidebar.title("DATABASE CONFIGURATION")
+st.sidebar.subheader("Enter MySQL connection details:", divider=True)
+user = st.sidebar.text_input("User", value=st.session_state.db_config['USER'])
+password = st.sidebar.text_input("Password", type="password", value=st.session_state.db_config['PASSWORD'])
+host = st.sidebar.text_input("Host", value=st.session_state.db_config['HOST'])
+port = st.sidebar.text_input("Port", value=st.session_state.db_config['PORT'])
 
 
 # # 3. Single dynamic button label.
